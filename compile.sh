787dd0566ca7c989034a952f07ab65c8502a87c6
apisitjoe <apisitjoe@gmail.com>
2022-12-18 13:20:39 +0700

    compile

---
+++ compile.sh
@@ -0,0 +1,2 @@
+#clang++ -Xclang -fopenmp -std=c++11 -Wall -o generator generator.cpp -O2 -lomp
+clang++ -Xclang -fopenmp -std=c++11 -Wall -o generator generator.cpp -O2 -lomp
--- generator.cpp
+++ generator.cpp
@@ -12,27 +12,40 @@
 #include <stack>
 #include <deque>
 #include <algorithm>
+
 #include <iostream>
 #include <omp.h>
+#include <parallel/algorithm>
+
+#define N 10000
 
 #define INDEX uint64_t
-#define DataType uint32_t
-#define DataMax UINT32_MAX
+
+//#define DataType uint32_t
+//#define DataType uint64_t
+#define DataType double
+//#define DataMax UINT32_MAX
+//#define DataMax UINT64_MAX
+#define DataMax DBL_MAX
+
 #define UNIQUE 100
 
 #define REVERSED 0
 #define NEARLYSORTED 0
 #define FEWUNIQUE 0
-
+void newinitData(DataType *arr, INDEX num){
+	srand(time(NULL));
+	__gnu_parallel::sort(&arr[left],&arr[right+1],__gnu_parallel::balanced_quicksort_tag());
+}
 void initData(DataType *arr,INDEX num){
-	
+	srand(time(NULL));
 #if REVERSED
 	for(INDEX i=0;i<num;i++){
         arr[i] = num-i;
     }
 #else
 	#if NEARLYSORTED
-	srand(time(NULL));
+	
 	for(INDEX i=0;i<num;i++){
 		if(i%7==1||i%7==3||i%7==5)
 			arr[i] = i-rand()%i;
@@ -49,12 +62,22 @@ void initData(DataType *arr,INDEX num){
         		arr[i] = unique[rand()%UNIQUE];
    	 		}
 		#else
-		
-    	srand(time(NULL));
+
     	for(INDEX i=0;i<num;i++){
         	arr[i] = rand()%DataMax;
    	 	}
    	 	#endif
     #endif
 #endif
+}
+
+int main(){
+	DataType* arr = (DataType*)malloc(sizeof(DataType)*N);
+	initData(arr,N);
+	
+	int i;
+	for(i=0;i<N;i++){
+		std::cout<<arr[i]<<" ";
+	}
+	return 0;
 }
