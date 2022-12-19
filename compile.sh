830cfa4cbf2fecc628da974b759b409e9069bc94
apisitjoe <apisitjoe@gmail.com>
2022-12-19 17:33:07 +0700

    AllTypes

--- compile.sh
+++ compile.sh
@@ -1,2 +1,3 @@
 #clang++ -Xclang -fopenmp -std=c++11 -Wall -o generator generator.cpp -O2 -lomp
-clang++ -Xclang -fopenmp -std=c++11 -Wall -o generator generator.cpp -O2 -lomp
+#clang++ -Xclang -fopenmp -std=c++11 -Wall -o generator generator.cpp -O2 -lomp
+g++-11 -fopenmp -std=c++11 -Wall -o generator generator.cpp -O2
--- generator.cpp
+++ generator.cpp
@@ -16,27 +16,116 @@
 #include <iostream>
 #include <omp.h>
 #include <parallel/algorithm>
+#include <cfloat>
+
+#define SORT_SWAP(x,y) {DataType __SORT_SWAP_t = (x); (x) = (y); (y) = __SORT_SWAP_t;}
 
 #define N 10000
 
 #define INDEX uint64_t
 
-//#define DataType uint32_t
-//#define DataType uint64_t
-#define DataType double
-//#define DataMax UINT32_MAX
-//#define DataMax UINT64_MAX
-#define DataMax DBL_MAX
+#define UINT32TYPE 1
+#define UINT64TYPE 0
+#define DOUBLETYPE 0
 
-#define UNIQUE 100
+#if UINT32TYPE
+#define DataType uint32_t
+#define DataMax UINT32_MAX
+#elif UINT64TYPE
+#define DataType uint64_t
+#define DataMax UINT64_MAX
+#elif DOUBLETYPE
+#define DataType double
+#define DataMax DBL_MAX
+#endif
+
+#define UNIQUENUM 100
+#define RANDSWAPPERCENT 99
 
 #define REVERSED 0
 #define NEARLYSORTED 0
 #define FEWUNIQUE 0
+
 void newinitData(DataType *arr, INDEX num){
 	srand(time(NULL));
-	__gnu_parallel::sort(&arr[left],&arr[right+1],__gnu_parallel::balanced_quicksort_tag());
+	
+	
+#if DOUBLETYPE
+	for(INDEX i=0;i<num;i++){
+		arr[i] = rand();
+	}
+#else 
+	INDEX max = DataMax;
+	INDEX inc = DataMax/num;
+	for(INDEX i=0;i<num;i++){
+		arr[i] = i*inc;
+	}
+#endif
+
+#if REVERSED
+	__gnu_parallel::sort(&arr[0],&arr[num-1],std::greater<DataType>(),__gnu_parallel::balanced_quicksort_tag());
+#else 
+	#if FEWUNIQUE
+		DataType unique[UNIQUENUM];
+		
+		#if DOUBLETYPE
+			for(INDEX i=0;i<UNIQUENUM;i++){
+				unique[i] = rand();
+			}
+			for(INDEX i=0;i<num;i++){
+        		arr[i] = unique[rand()%UNIQUENUM];
+   	 		}
+		#else 
+			for(INDEX i=0;i<UNIQUENUM;i++){
+				unique[i] = rand()%max;
+			}
+			for(INDEX i=0;i<num;i++){
+        		arr[i] = unique[rand()%UNIQUENUM];
+   	 		}
+		#endif
+	#else 
+		#if DOUBLETYPE
+			__gnu_parallel::sort(&arr[0],&arr[num-1],__gnu_parallel::balanced_quicksort_tag());
+		#else
+			
+		#endif
+			for(INDEX i=0;i<num*RANDSWAPPERCENT/100;i++){
+				SORT_SWAP(arr[rand()%num], arr[rand()%num]);
+			}
+	#endif
+#endif
+	
+	
+/*
+#if REVERSED
+	for(INDEX i=0;i<num;i++){
+        arr[i] = num-i;
+    }
+	
+#else
+	#if NEARLYSORTED
+	
+	#else
+		#if FEWUNIQUE
+			#if DOUBLETYPE
+	
+			#else 
+				DataType unique[UNIQUE];
+				for(INDEX i=0;i<UNIQUE;i++){
+					unique[i] = rand()%DataMax;
+				}
+		    	for(INDEX i=0;i<num;i++){
+        			arr[i] = unique[rand()%UNIQUE];
+   	 			}
+			#endif
+		#else //Random
+				
+		#endif
+	#endif
+#endif
+	*/
 }
+/*
 void initData(DataType *arr,INDEX num){
 	srand(time(NULL));
 #if REVERSED
@@ -70,11 +159,11 @@ void initData(DataType *arr,INDEX num){
     #endif
 #endif
 }
-
+*/
 int main(){
 	DataType* arr = (DataType*)malloc(sizeof(DataType)*N);
-	initData(arr,N);
-	
+	//initData(arr,N);
+	newinitData(arr, N);
 	int i;
 	for(i=0;i<N;i++){
 		std::cout<<arr[i]<<" ";
